a1a1ae3db8429aa5f9e3cec966f1f3f6cd49bda3
jaimin <jjjbhaduri@gmail.com>
2026-07-18 13:42:42 +0530

    scaffolding added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating directories for the project
+mkdir -p src
+mkdir -p research
+
+# Creating files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch research/requirements.txt
+
+echo "Project structure created successfully."
